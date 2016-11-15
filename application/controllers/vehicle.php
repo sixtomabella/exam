39a877e263795dcf4dba5a387296fabed06d49a2
Sixto Abella <sixjobs@gmail.com>
2016-11-15 18:52:25 +0800

    added new controller and model

---
+++ application/controllers/vehicle.php
@@ -0,0 +1,27 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Vehicle extends CI_Controller {
+
+    public function index()
+    {
+        $this->fetch();
+    }
+
+    public function fetch($id=''){
+
+    }
+
+    public function add(){
+
+    }
+
+    public function update(){
+
+    }
+
+    public function delete($id){
+
+    }
+}
---
+++ application/models/vehiclemodel.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kaye
+ * Date: 11/15/2016
+ * Time: 5:31 PM
+ * class descrption
+ * - model for retrieving, adding, updating, deleting info of vehicle list
+ */
+
+class vehiclemodel extends CI_Model {
+
+    /**
+     * initialize variables
+     */
+
+    private $ui = "";
+    private $name = "";
+    private $engine_displacement = "";
+    private $engine_power = "";
+
+
+
+
+
+
+
+} 
